8f87b23f167c3fda748e4f60fe2d927c9a63fa38
HwanHo Lee <leehh1412@hotmail.com>
2020-05-02 18:07:35 +0900

    초기 커밋.

---
+++ Asyvix.BlazorExample.SimpleTodo/Data/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asyvix.BlazorExample.SimpleTodo.Data
+{
+    public class TodoItem
+    {
+        public string Id { get; private set; } = Guid.NewGuid().ToString();
+        public string Content { get; set; } = string.Empty;
+        public bool IsComplete { get; set; } = false;
+    }
+}
